127178a8056e231677ae89685b9079506c07944f
Aaron Kirchhoff <askirchhoff@gmail.com>
2021-01-30 18:25:41 -0800

    january 30 commit

--- src/main/java/com/aaronkirchhoff/FruitStand/FruitStandApplication.java
+++ src/main/java/com/aaronkirchhoff/FruitStand/FruitStandApplication.java
@@ -1,7 +1,11 @@
 package com.aaronkirchhoff.FruitStand;
 
+import org.apache.catalina.connector.Connector;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FruitStandApplication {
@@ -10,4 +14,16 @@ public class FruitStandApplication {
 		SpringApplication.run(FruitStandApplication.class, args);
 	}
 
+//	@Bean
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//        Connector ajpConnector = new Connector("AJP/1.3");
+//        ajpConnector.setPort(9090);
+//        ajpConnector.setSecure(false);
+//        ajpConnector.setAllowTrace(false);
+//        ajpConnector.setScheme("http");
+//       ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+//        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+//    return tomcat;
+//    }
 }
